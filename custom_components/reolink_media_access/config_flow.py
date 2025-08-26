9f666188ec80082f7fd976fac2c6e97c98d158e5
root <root@DESKTOP-J66U7AS.localdomain>
2025-08-27 01:32:18 +0300

    Fix circular import: hardcode domain in config_flow instead of importing from __init__

--- custom_components/reolink_media_access/config_flow.py
+++ custom_components/reolink_media_access/config_flow.py
@@ -3,13 +3,11 @@ from __future__ import annotations
 from homeassistant import config_entries
 from homeassistant.data_entry_flow import FlowResult
 
-from . import DOMAIN
 
-
-class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+class ConfigFlow(config_entries.ConfigFlow, domain="reolink_media_access"):
 	VERSION = 1
 
 	async def async_step_user(self, user_input: dict | None = None) -> FlowResult:
-		await self.async_set_unique_id(DOMAIN)
+		await self.async_set_unique_id("reolink_media_access")
 		self._abort_if_unique_id_configured()
 		return self.async_create_entry(title="Reolink Media Access", data={})
